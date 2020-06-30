64d55158994fb51c79df2df912bf04dfab7edaec
Risyandi <Risyandi.94@gmail.com>
2020-06-30 17:50:40 +0700

    style(structure folder): update structure folder
    adding folder components and utils

---
+++ src/components/utils/api/configApi.js
@@ -0,0 +1,7 @@
+export default {
+    couchDBUrl: 'http://13.250.43.79:5984/', // 'http://localhost:5984'
+    couchDBAuth: {
+        username: 'admin',
+        password: 'iniadmin',
+    },
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app/app';
+import App from './components/app/app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
