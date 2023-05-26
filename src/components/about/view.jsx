c7bcaadbc1ef8f574837ffb781137bf7573e3ae4
Risyandi <risyandi.94@gmail.com>
2023-05-26 22:01:35 +0700

    fix: remove code not used

--- src/components/about/view.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Page from "../element/page";
-import Header from "./header";
-import Content from "./content";
-
-export default function AboutView() {
-    return <Page header={<Header />} content={<Content />} />;
-}
